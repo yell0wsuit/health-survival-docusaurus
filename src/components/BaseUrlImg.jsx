85ea9beef6aff985f99f4ad5bca91492af4ad531
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-02-18 00:10:20 +0700

    Update BaseUrlImg.jsx

--- src/components/BaseUrlImg.jsx
+++ src/components/BaseUrlImg.jsx
@@ -3,7 +3,7 @@ import useBaseUrl from "@docusaurus/useBaseUrl";
 
 const BaseUrlImg = ({ src, alt, ...props }) => (
     <a href="#" className="imgpop" data-bs-toggle="modal" data-bs-target="#imagemodal">
-        <img src={useBaseUrl(src)} className="img-fluid w-50 mx-auto d-block mb-4" alt={alt} {...props} loading="lazy" decoding="async" />
+        <img src={useBaseUrl(src)} className="figure-img img-fluid w-50 mx-auto d-block" alt={alt} {...props} loading="lazy" decoding="async" />
     </a>
 );
 
