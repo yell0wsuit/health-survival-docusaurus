bca14ed4825ff73313232ae004e5579a783dd82c
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-02-21 20:50:22 +0700

    Update ImageZoom.js

--- src/components/ImageZoom.js
+++ src/components/ImageZoom.js
@@ -1,50 +1,48 @@
-import React, { useState } from 'react';
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
+import React, { useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
 
 const ImageZoom = () => {
-  const [show, setShow] = useState(false);
-  const [imgSrc, setImgSrc] = useState('');
+    const [show, setShow] = useState(false);
+    const [imgSrc, setImgSrc] = useState("");
 
-  const handleClose = () => setShow(false);
-  const handleShow = (src) => {
-    setImgSrc(src);
-    setShow(true);
-  };
-
-  // Attach the event listener to a static parent element in useEffect
-  React.useEffect(() => {
-    const handleClick = (event) => {
-      let targetElement = event.target.closest('.imgpop');
-
-      if (targetElement && targetElement.nodeName === 'A') {
-        event.preventDefault();
-        const imgSrc = targetElement.querySelector('img').src;
-        handleShow(imgSrc);
-      }
+    const handleClose = () => setShow(false);
+    const handleShow = (src) => {
+        setImgSrc(src);
+        setShow(true);
     };
 
-    document.body.addEventListener('click', handleClick);
+    // Attach the event listener to a static parent element in useEffect
+    React.useEffect(() => {
+        const handleClick = (event) => {
+            let targetElement = event.target.closest(".imgpop");
 
-    return () => {
-      document.body.removeEventListener('click', handleClick);
-    };
-  }, []);
+            if (targetElement && targetElement.nodeName === "A") {
+                event.preventDefault();
+                const imgSrc = targetElement.querySelector("img").src;
+                handleShow(imgSrc);
+            }
+        };
 
-  return (
-    <Modal show={show} onHide={handleClose}
-      size="xl"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-    >
-      <Modal.Body>
-        <img src={imgSrc} id="imagepreview" className="img-fluid mx-auto d-block" />
-      </Modal.Body>
-      <Modal.Footer className="border-0">
-        <Button variant="secondary" onClick={handleClose}>Close</Button>
-      </Modal.Footer>
-    </Modal>
-  );
+        document.body.addEventListener("click", handleClick);
+
+        return () => {
+            document.body.removeEventListener("click", handleClick);
+        };
+    }, []);
+
+    return (
+        <Modal show={show} onHide={handleClose} size="xl" aria-labelledby="contained-modal-title-vcenter" centered>
+            <Modal.Body>
+                <img src={imgSrc} id="imagepreview" className="img-fluid mx-auto d-block" />
+            </Modal.Body>
+            <Modal.Footer className="border-0">
+                <Button variant="secondary" onClick={handleClose}>
+                    Close
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    );
 };
 
 export default ImageZoom;
