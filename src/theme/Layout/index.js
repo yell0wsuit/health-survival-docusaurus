5bd0cf8361ed7634fe18956ef18818146d2d2ac4
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-02-26 22:25:19 +0700

    @vercel/speed-insights/react !!!!!!!!
    next.js lol

--- src/theme/Layout/index.js
+++ src/theme/Layout/index.js
@@ -1,54 +1,54 @@
-import React from 'react';
-import OriginalLayout from '@theme-original/Layout';
-import NProgress from 'nprogress';
-import 'nprogress/nprogress.css';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import OriginalLayout from "@theme-original/Layout";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+import { useHistory } from "react-router-dom";
 
-import ImageZoom from '../../components/ImageZoom';
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import ImageZoom from "../../components/ImageZoom";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const LayoutWrapper = (props) => {
-  const history = useHistory();
+    const history = useHistory();
 
-  React.useEffect(() => {
-    NProgress.configure({ showSpinner: true });
-  
-    // Start NProgress before a route change
-    const startNProgress = () => NProgress.start();
-  
-    // Stop NProgress after a route change
-    const stopNProgress = () => NProgress.done();
-  
-    // Listen for route changes
-    history.listen(() => {
-      startNProgress();
-      // Consider removing the timeout here and relying on the subsequent page's load event
-      // or another signal from Docusaurus when the page is fully rendered
-    });
-  
-    // Start NProgress on initial load
-    startNProgress();
-    stopNProgress();
-  
-    // Attach to window load event for the initial load
-    const handleLoad = () => {
-      stopNProgress();
-    };
-    window.addEventListener('load', handleLoad);
-  
-    return () => {
-      window.removeEventListener('load', handleLoad);
-      NProgress.remove();
-    };
-  }, [history]);  
+    React.useEffect(() => {
+        NProgress.configure({ showSpinner: true });
 
-  return (
-    <>
-      <OriginalLayout {...props} />
-      <ImageZoom />
-      <SpeedInsights />
-    </>
-  );
+        // Start NProgress before a route change
+        const startNProgress = () => NProgress.start();
+
+        // Stop NProgress after a route change
+        const stopNProgress = () => NProgress.done();
+
+        // Listen for route changes
+        history.listen(() => {
+            startNProgress();
+            // Consider removing the timeout here and relying on the subsequent page's load event
+            // or another signal from Docusaurus when the page is fully rendered
+        });
+
+        // Start NProgress on initial load
+        startNProgress();
+        stopNProgress();
+
+        // Attach to window load event for the initial load
+        const handleLoad = () => {
+            stopNProgress();
+        };
+        window.addEventListener("load", handleLoad);
+
+        return () => {
+            window.removeEventListener("load", handleLoad);
+            NProgress.remove();
+        };
+    }, [history]);
+
+    return (
+        <>
+            <OriginalLayout {...props} />
+            <ImageZoom />
+            <SpeedInsights />
+        </>
+    );
 };
 
 export default LayoutWrapper;
