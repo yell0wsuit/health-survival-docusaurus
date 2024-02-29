69ec8185b0c7cac3de387d65edffccf9e38c411d
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-02-29 16:12:54 +0700

    i18n: fix complains

--- src/i18n.js
+++ src/i18n.js
@@ -3,17 +3,31 @@ import { initReactI18next } from "react-i18next";
 import HttpBackend from "i18next-http-backend";
 import LanguageDetector from "i18next-browser-languagedetector";
 
+import translationEN from "/lang/en.json";
+import translationVI from "/lang/vi.json";
+
+const resources = {
+    en: {
+        translation: translationEN,
+    },
+    vi: {
+        translation: translationVI,
+    },
+};
+
 i18n.use(HttpBackend)
     .use(LanguageDetector)
     .use(initReactI18next)
     .init({
+        resources,
+        supportedLngs: ["en", "vi"],
         fallbackLng: "en",
         debug: true,
         interpolation: {
             escapeValue: false,
         },
         backend: {
-            loadPath: "/lang/{{lng}}.json",
+            loadPath: "lang/{{lng}}.json",
         },
     });
 
