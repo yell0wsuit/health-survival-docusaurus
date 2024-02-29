5bd0ea8d68badf3aedd4bea737cfa26f4e566b17
yell0wsuit <5692900+yell0wsuit@users.noreply.github.com>
2024-02-29 15:58:33 +0700

    Fix carousel on iPad Air screens

--- src/components/HomePageCarousel.jsx
+++ src/components/HomePageCarousel.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Carousel, Row, Col, Card, Button } from "react-bootstrap";
+import { Carousel, Row, Col, Card, Button, Ratio } from "react-bootstrap";
 
 function HomePageCarousel() {
     const [index, setIndex] = useState(0);
@@ -8,7 +8,6 @@ function HomePageCarousel() {
     const handleSelect = (selectedIndex, e) => {
         setIndex(selectedIndex);
     };
-    
 
     const goToPrev = () => {
         setIndex(index === 0 ? totalSlides - 1 : index - 1);
@@ -31,39 +30,45 @@ function HomePageCarousel() {
             <Carousel fade indicators={false} id="cardCarousel" controls={false} activeIndex={index} onSelect={handleSelect}>
                 <Carousel.Item>
                     <Row>
-                        <Col md={3} lg={4} className="d-flex mb-3">
+                        <Col md={4} className="d-flex mb-3">
                             <Card>
-                                <Card.Img variant="top" src="/images/apple-health/Eucalyptus-Sleep_Article_illustration.jpg" />
+                                <Ratio aspectRatio="16x9">
+                                    <Card.Img variant="top" src="/images/apple-health/Eucalyptus-Sleep_Article_illustration.jpg" />
+                                </Ratio>
                                 <Card.Body>
                                     <Card.Title className="fw-semibold">I have trouble sleeping</Card.Title>
-                                    <Card.Text>
-                                        If you can't sleep well, consider these tips.
-                                    </Card.Text>
-                                    <Button variant="primary" href="/apple-health/getting-a-good-nights-sleep">Read...</Button>
+                                    <Card.Text>If you can't sleep well, consider these tips.</Card.Text>
+                                    <Button variant="primary" href="/apple-health/getting-a-good-nights-sleep">
+                                        Read...
+                                    </Button>
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col md={3} lg={4} className="d-flex mb-3">
+                        <Col md={4} className="d-flex mb-3">
                             <Card>
-                                <Card.Img variant="top" src="/images/apple-health/MoodsEmotion_Article_Illustration.jpg" />
+                                <Ratio aspectRatio="16x9">
+                                    <Card.Img variant="top" src="/images/apple-health/MoodsEmotion_Article_Illustration.jpg" />
+                                </Ratio>
                                 <Card.Body>
                                     <Card.Title className="fw-semibold">What's the difference between emotions and moods?</Card.Title>
-                                    <Card.Text>
-                                        While it’s easy to use these terms interchangeably, there are key differences.
-                                    </Card.Text>
-                                    <Button variant="primary" href="/apple-health/the-difference-between-emotion-and-mood">Read...</Button>
+                                    <Card.Text>While it’s easy to use these terms interchangeably, there are key differences.</Card.Text>
+                                    <Button variant="primary" href="/apple-health/the-difference-between-emotion-and-mood">
+                                        Read...
+                                    </Button>
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col md={3} lg={4} className="d-flex mb-3">
+                        <Col md={4} className="d-flex mb-3">
                             <Card>
-                                <Card.Img variant="top" src="/images/sas-urban/wildfire1.jpg" />
+                                <Ratio aspectRatio="16x9">
+                                    <Card.Img variant="top" src="/images/sas-urban/wildfire1.jpg" />
+                                </Ratio>
                                 <Card.Body>
                                     <Card.Title className="fw-semibold">Give me some fire facts</Card.Title>
-                                    <Card.Text>
-                                        Remembering the "fire triangle" idea will help you one day.
-                                    </Card.Text>
-                                    <Button variant="primary" href="/sas-urban/urban-fire#fire-facts">Read...</Button>
+                                    <Card.Text>Remembering the "fire triangle" idea will help you one day.</Card.Text>
+                                    <Button variant="primary" href="/sas-urban/urban-fire#fire-facts">
+                                        Read...
+                                    </Button>
                                 </Card.Body>
                             </Card>
                         </Col>
@@ -71,39 +76,50 @@ function HomePageCarousel() {
                 </Carousel.Item>
                 <Carousel.Item>
                     <Row>
-                        <Col md={3} lg={4} className="d-flex mb-3">
+                        <Col md={4} className="d-flex mb-3">
                             <Card>
-                                <Card.Img variant="top" src="/images/sas-urban/1537827855952.webp" />
+                                <Ratio aspectRatio="16x9">
+                                    <Card.Img variant="top" src="/images/sas-urban/1537827855952.webp" />
+                                </Ratio>
                                 <Card.Body>
                                     <Card.Title className="fw-semibold">Terrorism</Card.Title>
-                                    <Card.Text>
-                                        What you should do to survive when being held hostage.
-                                    </Card.Text>
-                                    <Button variant="primary" href="/sas-urban/urban-terrorism">Read...</Button>
+                                    <Card.Text>What you should do to survive when being held hostage.</Card.Text>
+                                    <Button variant="primary" href="/sas-urban/urban-terrorism">
+                                        Read...
+                                    </Button>
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col md={3} lg={4} className="d-flex mb-3">
+                        <Col md={4} className="d-flex mb-3">
                             <Card>
-                                <Card.Img variant="top" src="/images/sas-urban/Community_Transit_15814_Double_Tall_in_Seattle-scaled.jpg" />
+                                <Ratio aspectRatio="16x9">
+                                    <Card.Img variant="top" src="/images/sas-urban/Community_Transit_15814_Double_Tall_in_Seattle-scaled.jpg" />
+                                </Ratio>
                                 <Card.Body>
                                     <Card.Title className="fw-semibold">Travelling abroad</Card.Title>
                                     <Card.Text>
-                                        As soon as you enter a foreign country, you face many differences—language, climate, customs and laws which may vary widely from those you take for granted.
+                                        As soon as you enter a foreign country, you face many differences—language, climate, customs and laws which
+                                        may vary widely from those you take for granted.
                                     </Card.Text>
-                                    <Button variant="primary" href="/sas-urban/urban-in-transit#travelling-abroad">Read...</Button>
+                                    <Button variant="primary" href="/sas-urban/urban-in-transit#travelling-abroad">
+                                        Read...
+                                    </Button>
                                 </Card.Body>
                             </Card>
                         </Col>
-                        <Col md={3} lg={4} className="d-flex mb-3">
+                        <Col md={4} className="d-flex mb-3">
                             <Card>
-                                <Card.Img variant="top" src="/images/sas-survival/essentials_hero.webp" />
+                                <Ratio aspectRatio="16x9">
+                                    <Card.Img variant="top" src="/images/sas-survival/essentials_hero.webp" />
+                                </Ratio>
                                 <Card.Body>
                                     <Card.Title className="fw-semibold">Making plans for surviving in the wild</Card.Title>
                                     <Card.Text>
-                                        Preparing yourself to be a survivor. And did you know a knife is one of the most important essential tools? 
+                                        Preparing yourself to be a survivor. And did you know a knife is one of the most important essential tools?
                                     </Card.Text>
-                                    <Button variant="primary" href="/sas-survival-guide/ssg-essentials">Read...</Button>
+                                    <Button variant="primary" href="/sas-survival-guide/ssg-essentials">
+                                        Read...
+                                    </Button>
                                 </Card.Body>
                             </Card>
                         </Col>
